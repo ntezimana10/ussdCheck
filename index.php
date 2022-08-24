3a9e7655c47e650a52b8947c070c32900dc66ebf
ntezimana10 <111853410+ntezimana10@users.noreply.github.com>
2022-08-24 01:01:44 -0700

    Update index.php

--- index.php
+++ index.php
@@ -4,34 +4,34 @@ $sessionId=$_POST["sessionId"];
 $serviceCode=$_POST["serviceCode"];
 $phoneNumber=$_POST["phoneNumber"];
 $text=$_POST["text"];
-if($text==""){
+if ($text==""){
 
-$response= "CON what would you want to check \n";
-$response.= "1. My Account NO \n";
-$response.= "2. My Account balance";
+$response = "CON what would you want to check \n";
+$response .= "1. My Account NO \n";
+$response .= "2. My Account balance";
 }
-else if($text=="1"){
+else if ($text == "1"){
     
     $response= "CON choose account information you want to view \n";
-$response.= "1. My Account NO \n";
-$response.= "2. My Account balance";
+$response .= "1. My Account NO \n";
+$response .= "2. My Account balance";
 }
-else if($text=="2"){
+else if ($text == "2"){
     
-    $response="END your fone number is".$phoneNumber;
+    $response = "END your fone number is".$phoneNumber;
 }
-else if($text=="1*1"){
+else if ($text == "1*1"){
     
-    $accountNumber="acc000111";
+    $accountNumber = "acc000111";
     
     $response="END your account number is".$accountNumber; 
 }
-else if($text=="1*2"){
+else if($text == "1*2"){
 
 $balance="rwf 50000";
 
 
-$response="END your balance is".$balance;
+$response = "END your balance is".$balance;
 }
 
 header('Content-type; text/plain');
