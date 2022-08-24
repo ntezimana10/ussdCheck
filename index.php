bf1f6d3013010cd64721b1e228bf1b046785dd74
ntezimana10 <111853410+ntezimana10@users.noreply.github.com>
2022-08-24 01:13:11 -0700

    Update index.php

--- index.php
+++ index.php
@@ -1,38 +1,42 @@
-<?php
+
+<DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SU/IPRC NGOMA VOTING SYSTEM</title>
+    <link rel="stylesheet" href="style/css/bootstrap.min.css">
+    <style>
+      body{
+background-color:rgb(248, 248, 248);
+border: 6px solid grey;
+ 
+}
+      label{
+        font-family:Arial;
+        font-size:18px;
+      }
+    </style>
+</head>
+<body>
   
-$sessionId=$_POST["sessionId"];
-$serviceCode=$_POST["serviceCode"];
-$phoneNumber=$_POST["phoneNumber"];
-$text=$_POST["text"];
-if ($text==""){
-
-$response = "CON what would you want to check \n";
-$response .= "1. My Account NO \n";
-$response .= "2. My Account balance";
-}
-else if ($text == "1"){
-    
-    $response= "CON choose account information you want to view \n";
-$response .= "1. My Account NO \n";
-$response .= "2. My Account balance";
-}
-else if ($text == "2"){
-    
-    $response = "END your fone number is".$phoneNumber;
-}
-else if ($text == "1*1"){
-    
-    $accountNumber = "acc000111";
-    
-    $response="END your account number is".$accountNumber; 
-}
-else if($text == "1*2"){
-
-$balance="rwf 50000";
-
-
-$response = "END your balance is".$balance;
-}
+      <h3 align="center">Register Board Units</h3>
+      <br><br>
+   <div class="container" style="background-color:;width: 500px;height: 240px; border: 1px solid green;margin-top:3px;">
+      <form action="#" method="POST">
+      
+        <label style="margin-right: 10px;margin-top:40px;">Board unit</label>
+         <select name="board_name"  class="form-control">
+  <Option value="executive_committee">executive_committee</Option>
+  <Option value="arbitration_committee">arbitration_committee</Option>
+  <Option value="representative_association_board">representative_association_board</Option>
+</select><br><br>
+        <input type="submit" value="regiser"  name=" send" class="btn btn-primary btn-block" style="width:100%;" >
+       <br>
+      </form>
+     </div>
+</body>
+</html> 
 
 header('Content-type; text/plain');
 echo $response;
